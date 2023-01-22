def3833ad0438ebbece69894c7132613cd02ab8f
unknown <rayarezaie83@gmail.com>
2023-01-22 13:56:23 +0330

    createfile and cat

--- firstproject.c
+++ firstproject.c
@@ -4,6 +4,8 @@
 #include<sys/stat.h>
 #include<dirent.h>
 #include<errno.h>
+
+
 void createfile(char *address)
 {    
 FILE *fptr = fopen(address, "w");
@@ -37,11 +39,30 @@ char** str_chop(const char* inp, const char* space, int* num)
     }
     *num = inp_num;
     return words;
-} 
+} /* 
+void cat(char *filename)
+{
+    char read ;
+    FILE *file ;
+    if (file = fopen("a.txt", "r"))
+    {
+     do {
+        read = fgetc(file);
+        printf("%c", read);
+ 
+    
+    } while (read != EOF);
+}
+}
+fclose(file);
+//else
+printf("The file doesnt exist"); */
 int main(){
-    //mkdir("d:tototo1");
+    //mkdir("d:/root");
     char command [105] ;
     int num;     //number of words of a command
+    printf("(Enter \"help\" for list of commands)\n\n");
+    printf("Command: ");
     fgets(command , 105 , stdin);
     char ** word= str_chop(command, " " , &num);
     for ( int i = 0 ; i < num ; i ++){
@@ -51,16 +72,20 @@ int main(){
     if ( strcmp(word[0] ,"createfile") == 0 ){
         int dir_num ;
         char ** dir = str_chop(word[2] , "/" , &dir_num) ;
+
         for ( int i = 0 ; i < dir_num - 1 ; i++)
         {
-            DIR *directory = dir[i] ;
+            //printf("%s",dir[i]);
+            /* DIR *directory = dir[i] ;
             if ( opendir(directory))  
             break;
             else if (ENOENT == errno)
-            mkdir(directory) ;
+            mkdir(directory) ; */
           }
         
-        createfile(word[2]);
+        //createfile(word[2]);
 
     }   
+    //if (strcmp(word[0], "cat") == 0)
+    //cat(word[2]);
 }
